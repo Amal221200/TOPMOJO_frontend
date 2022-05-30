9518dafd8b70184ec544d4ed40f4ecdf5825c532
Amal221200 <amalmurikkoli2000@gmail.com>
2022-05-30 14:22:28 +0530

    nav /blogs

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,24 @@ import '../styles/globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Head from 'next/head'
-import {useRouter} from 'next/router'
-import { ThemeProvider } from 'next-themes'
+import { useRouter } from 'next/router'
+import { ThemeProvider, useTheme } from 'next-themes'
 import LoadingBar from 'react-top-loading-bar'
 import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   const [progress, setProgress] = useState(0)
+  const { theme, setTheme } = useTheme()
 
-  useEffect(()=> {
-    router.events.on('routeChangeStart', ()=> {
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
       setProgress(40)
     })
-    router.events.on('routeChangeComplete', ()=> {
+    router.events.on('routeChangeComplete', () => {
       setProgress(100)
     })
+    setTheme(localStorage.getItem('theme') !== null && localStorage.getItem('theme'))
   }, [])
 
   return (
@@ -25,7 +27,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>TopMojo</title>
       </Head>
-      <LoadingBar color='#eb3b3b' progress={progress} onLoaderFinished={()=> {setProgress(0)}} height={2} waitingTime={400} />
+      <LoadingBar color='#eb3b3b' progress={progress} onLoaderFinished={() => { setProgress(0) }} height={2} waitingTime={400} />
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -8,7 +8,7 @@ const Blogs = ({ blogs, message }) => {
                 <div className='flex flex-wrap'>
                     {!message ? (
                         blogs.map(blog => (
-                            <div key={blog.id} className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 lg:m-0 m-2 border-l-2 border-gray-200 border-opacity-60">
+                            <div key={blog.id} className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 lg:m-0 m-2 border-l-2 border-gray-800 dark:border-gray-200 border-opacity-60">
                                 <h2 className="text-lg sm:text-xl text-gray-900 dark:text-neutral-500 font-medium title-font md:mb-4 mb-2">{blog.attributes.title}</h2>
                                 <p className="leading-relaxed text-base mb-4 text-black dark:text-white lg:flex lg:items-center lg:min-h-[15rem]">{blog.attributes.description}</p>
                                 <Link href={`/blogs/${blog.attributes.slug.data.attributes.slug}`}>
