309f06712fa5ddb829e75326d63c05797e4b0f65
Amal221200 <amalmurikkoli2000@gmail.com>
2022-05-30 11:18:41 +0530

    loader

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,30 @@ import '../styles/globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Head from 'next/head'
-import { useEffect } from 'react'
-import { ThemeProvider, useTheme } from 'next-themes'
+import {useRouter} from 'next/router'
+import { ThemeProvider } from 'next-themes'
+import LoadingBar from 'react-top-loading-bar'
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const [progress, setProgress] = useState(0)
 
+  useEffect(()=> {
+    router.events.on('routeChangeStart', ()=> {
+      setProgress(40)
+    })
+    router.events.on('routeChangeComplete', ()=> {
+      setProgress(100)
+    })
+  }, [])
 
   return (
     <ThemeProvider attribute='class'>
       <Head>
         <title>TopMojo</title>
       </Head>
+      <LoadingBar color='#eb3b3b' progress={progress} onLoaderFinished={()=> {setProgress(0)}} height={2} waitingTime={400} />
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import BlogList from '../components/BlogList'
 
 export default function Home({ blogs, web_desc, message }) {
@@ -8,7 +7,7 @@ export default function Home({ blogs, web_desc, message }) {
     <section className="text-gray-600 body-font min-h-screen">
       {!message ? (
         <>
-          <main className={`relative my-0 mx-auto max-w-[100vw] flex justify-center flex-col min-h-[90vh] items-center z-10 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-[url(../public/main-bg.jpg)] before:dark:bg-[url(../public/main-bg-dark.jpg)] before:object-center before:bg-center before:bg-cover before:opacity-80 before:-z-10`}>
+          <main className={`relative my-0 mx-auto max-w-[100vw] flex justify-center flex-col min-h-[90vh] md:min-h-screen items-center z-10 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-[url(../public/main-bg.jpg)] before:dark:bg-[url(../public/main-bg-dark.jpg)] before:object-center before:bg-center before:bg-cover before:opacity-80 before:-z-10`}>
             <h2 className={`my-4 font-bold text-4xl text-black dark:text-[#eb3b3b]`}>{web_desc.attributes.title}</h2>
             <p className={`leading-7 text-orange-700 dark:text-red-600 text-lg md:text-2xl font-semibold px-5`}>{web_desc.attributes.description}</p>
           </main>
@@ -22,7 +21,7 @@ export default function Home({ blogs, web_desc, message }) {
             </div>
           </div>
         </>
-      ) : <h1>message</h1>
+      ) : <h1 className='text-center text-xl mt-4'>message</h1>
       }
     </section>
   )
