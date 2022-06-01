f100e16630afb9ab2a0325821a4445fbb728803a
Amal221200 <amalmurikkoli2000@gmail.com>
2022-06-01 13:38:15 +0530

    Post and Myself

--- pages/myself/index.js
+++ pages/myself/index.js
@@ -7,11 +7,11 @@ const Myself = ({message, blog}) => {
     })
     const htmlContent = md.render(blog.attributes.myself)
   return (
-    <div className='max-w-screen-xl container min-h-screen px-7 mt-8 mx-auto prose prose-headings:text-center dark:prose-headings:text-gray-400 dark:prose-p:text-white dark:prose-strong:text-gray-500'>
+    <section className='max-w-screen-xl container min-h-screen px-7 mt-8 mx-auto prose prose-headings:font-semibold prose-headings:text-center dark:prose-headings:text-gray-400 dark:prose-p:text-gray-200 dark:prose-strong:text-white'>
         {!message ? (
             <div dangerouslySetInnerHTML={{__html: htmlContent}}></div>
         ): (<h1>{message}</h1>)}
-    </div>
+    </section>
   )
 }
 
