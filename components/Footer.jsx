578008ca7e94b41206a5fb31d3394de2ae3114b0
Amal221200 <amalmurikkoli2000@gmail.com>
2022-06-03 19:38:48 +0530

    little changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
     return (
         <footer className='sm:p-4 p-3 bg-[#eb3b3b]'>
-            <h2 className='text-center text-lg'>Topmojo | Copyright &copy; 2022</h2>
+            <h2 className='text-center text-lg text-black dark:text-white'><Link  href="/"><a className='text-black dark:text-white'>Topmojo</a></Link> | Copyright &copy; 2022</h2>
         </footer>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ blogs, web_desc, message }) {
   return (
     <section className="text-gray-600 body-font min-h-screen">
       {!message ? (
-        <section>
+        <>
           <main className={`relative my-0 mx-auto max-w-[100vw] flex justify-center flex-col min-h-[90vh] md:min-h-screen items-center z-10 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-[url(../public/main-bg.jpg)] before:dark:bg-[url(../public/main-bg-dark.jpg)] before:object-center before:bg-center before:bg-cover before:opacity-80 before:-z-10`}>
             <h2 className={`my-4 font-bold text-4xl text-black dark:text-[#eb3b3b]`}>{web_desc.attributes.title}</h2>
             <p className={`leading-7 text-orange-700 dark:text-red-600 text-lg md:text-2xl font-semibold px-5`}>{web_desc.attributes.description}</p>
@@ -29,7 +29,7 @@ export default function Home({ blogs, web_desc, message }) {
               </Swiper>
             </div>
           </div>
-        </section>
+        </>
       ) : <h1 className='text-center text-xl mt-4'>message</h1>
       }
     </section>
